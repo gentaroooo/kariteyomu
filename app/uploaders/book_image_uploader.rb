4aeb47ead705d1c24e86a19ff2e2a3bc734c6f8e
gentaro <going3956@yahoo.co.jp>
2022-09-08 10:27:49 +0900

    git add: アップローダーの作成

---
+++ app/uploaders/book_image_uploader.rb
@@ -0,0 +1,15 @@
+class BookImageUploader < CarrierWave::Uploader::Base
+
+  def store_dir
+    "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+  end
+
+  def default_url
+    'board_placeholder.png'
+  end
+
+  def extension_whitelist
+    %w(jpg jpeg gif png)
+  end
+
+end
