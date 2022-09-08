cc4257d21b6e644ad7d7c1f63c6dc2a1b01e750a
gentaro <going3956@yahoo.co.jp>
2022-09-08 10:30:38 +0900

    git add: bookテーブルにboard_imageカラムの追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_09_07_052630) do
+ActiveRecord::Schema.define(version: 2022_09_08_012918) do
 
   create_table "books", force: :cascade do |t|
     t.string "title"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 2022_09_07_052630) do
     t.integer "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "book_image"
     t.index ["user_id"], name: "index_books_on_user_id"
   end
 
