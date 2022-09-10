a89ec7de2570bb1818ff7cda05071a35891d0cff
gentaro <going3956@yahoo.co.jp>
2022-09-10 17:14:12 +0900

    add: google_books_apiをviewで表示するためのメソッド

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+  module ApplicationHelpe
+    def google_book_thumbnail(google_book)
+      google_book['volumeInfo']['imageLinks'].nil? ? 'sample.jpg' : google_book['volumeInfo']['imageLinks']['thumbnail']
+    end
+  
+    def set_google_book_params(google_book)
+      google_book['volumeInfo']['bookImage'] = google_book.dig('volumeInfo', 'imageLinks', 'thumbnail')
+      google_book['volumeInfo'].slice('title', 'authors', 'publishedDate', 'infoLink', 'bookImage')
+    end
+  end
 end
