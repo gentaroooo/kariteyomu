83080b6b1103b1b49fa11818e9f2d97ed07262e1
gentaro <going3956@yahoo.co.jp>
2022-09-16 21:21:52 +0900

    add: postモデル

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_09_10_204529) do
+ActiveRecord::Schema.define(version: 2022_09_16_121949) do
 
   create_table "authors", force: :cascade do |t|
     t.string "name", null: false
@@ -52,6 +52,15 @@ ActiveRecord::Schema.define(version: 2022_09_10_204529) do
     t.index ["user_id"], name: "index_comments_on_user_id"
   end
 
+  create_table "posts", force: :cascade do |t|
+    t.string "title", null: false
+    t.text "body", null: false
+    t.integer "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_posts_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", null: false
     t.string "crypted_password"
@@ -73,4 +82,5 @@ ActiveRecord::Schema.define(version: 2022_09_10_204529) do
   add_foreign_key "books", "users"
   add_foreign_key "comments", "books"
   add_foreign_key "comments", "users"
+  add_foreign_key "posts", "users"
 end
