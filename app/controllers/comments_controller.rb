9896cddc0656670aa0e0765053245ea1f0d9a772
gentaro <going3956@yahoo.co.jp>
2022-09-18 09:00:51 +0900

    fix: コントローラーのコメント機能をPOST用に修正

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -12,6 +12,6 @@ class CommentsController < ApplicationController
   private
 
   def comment_params
-    params.require(:comment).permit(:body).merge(book_id: params[:book_id])
+    params.require(:comment).permit(:body).merge(post_id: params[:post_id])
   end
 end
