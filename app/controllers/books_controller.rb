725c7a69901100b3af9007235b53dc3c480158ec
gentaro <going3956@yahoo.co.jp>
2022-09-21 05:27:27 +0900

    add: カーリルAPI連携で接続失敗を考慮

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -26,15 +26,37 @@ class BooksController < ApplicationController
     @book = Book.find(params[:id])
 
     uri = URI.parse("https://api.calil.jp/check")
-    q = {appkey: ENV['APP_KEY'],
-        isbn: @book.systemid,
-        systemid: "Kanagawa_Hiratsuka",
-        callback: :no}
 
+    # q = {appkey: ENV['APP_KEY'],
+    #     isbn: @book.systemid,
+    #     systemid: "Kanagawa_Hiratsuka",
+    #     callback: :no}
+
+    q = {appkey: ENV['APP_KEY'], callback: :no}
+    q.merge!({isbn: @book.systemid})
+    q.merge!({systemid: "Kanagawa_Hiratsuka"})
     uri.query = URI.encode_www_form(q)
-    response = Net::HTTP.get_response(uri)
-    @hash = JSON.parse(response.body)["books"]["#{@book.systemid.to_i}"]["Kanagawa_Hiratsuka"]["libkey"].to_a
+    response = hash = nil
 
+    loop do
+      response = Net::HTTP.get_response(uri)
+      @hash = JSON.parse(response.body)["books"]["#{@book.systemid.to_i}"]["Kanagawa_Hiratsuka"]["libkey"].to_a
+      test = JSON.parse(response.body)
+      break if test["continue"].zero?
+      puts "continue"
+      sleep(3)
+      q = {appkey: ENV['APP_KEY'], session: test["session"], callback: :no}
+      uri.query = URI.encode_www_form(q)
+    end
+    
+    puts response.code
+    pp hash
+
+
+    # uri.query = URI.encode_www_form(q)
+    # response = Net::HTTP.get_response(uri)
+    # @hash = JSON.parse(response.body)["books"]["#{@book.systemid.to_i}"]["Kanagawa_Hiratsuka"]["libkey"].to_a
+    # binding.pry
   end
 
   def edit; end
