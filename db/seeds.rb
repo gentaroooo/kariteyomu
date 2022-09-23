2497ccf20a230d90b148c4effc9196111d3bc35b
gentaro <going3956@yahoo.co.jp>
2022-09-23 10:04:30 +0900

    add: seedにカテゴリーを追加

--- db/seeds.rb
+++ db/seeds.rb
@@ -22,3 +22,14 @@ Post.create(
     body: "本文#{index}"
   )
 end
+
+Category.create([
+  { name: '0歳' },
+  { name: '1歳' },
+  { name: '2歳' },
+  { name: '3歳' },
+  { name: '4歳' },
+  { name: '5歳' },
+  { name: '6歳' },
+  { name: '大人' },
+])
