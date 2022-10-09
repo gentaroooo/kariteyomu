e5bfabc3381d40195c22f05b67a0380679388e17
gentaro <going3956@yahoo.co.jp>
2022-10-09 13:15:33 +0900

    add: library関連のアソシエーション

--- app/models/library.rb
+++ app/models/library.rb
@@ -1,2 +1,7 @@
 class Library < ApplicationRecord
+  has_many :user_libraries, dependent: :destroy
+  has_many :users, through: :user_libraries, source: :user
+
+  validates :name, presence: true
 end
+
