c12c85c8485d5b880fdd7496a26b34686d3008f8
gentaro <going3956@yahoo.co.jp>
2022-10-28 00:15:13 +0900

    add: レビューから読みたいへ登録可能に変更

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -38,6 +38,7 @@ class PostsController < ApplicationController
   end
 
   def show
+    @book = Book.new
     @post = Post.find(params[:id])
     @comment = Comment.new
     @comments = @post.comments.includes(:user).order(created_at: :desc)
@@ -91,12 +92,11 @@ class PostsController < ApplicationController
   end
 
   def authors_params
-      if params.dig(:book).nil?
-        params.permit(authors: ['著者不明'])
-      else
-        params.require(:book).permit(authors: [])
-      end
-       
+    if params.dig(:book).nil?
+      params.permit(authors: ['著者不明'])
+    else
+      params.require(:book).permit(authors: [])
+    end       
   end
 
   def set_volume_info
