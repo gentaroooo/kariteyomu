92d363d0c45817175c71f77df9c88944c5cda658
gentaro <going3956@yahoo.co.jp>
2022-10-30 02:36:13 +0900

    add: cloudinary4

--- app/uploaders/avatar_uploader.rb
+++ app/uploaders/avatar_uploader.rb
@@ -3,11 +3,9 @@ class AvatarUploader < CarrierWave::Uploader::Base
   # include CarrierWave::RMagick
   # include CarrierWave::MiniMagick
 
-  if Rails.env.production?
-    include Cloudinary::CarrierWave
-  else
-    storage :file
-  end
+  # Choose what kind of storage to use for this uploader:
+  storage :file
+  # storage :fog
 
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
