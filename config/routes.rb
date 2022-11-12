9e9663df7ad3ac7e8e5a35e03f21ac39a839a64a
gentaro <going3956@yahoo.co.jp>
2022-11-12 20:30:15 +0900

    add: Admin用ルートを追加

--- config/routes.rb
+++ config/routes.rb
@@ -39,4 +39,12 @@ Rails.application.routes.draw do
   resources :password_resets, only: %i[new create edit update]
   resources :relationships, only: [:create, :destroy]
   resources :libraries, only: [:index, :create, :destroy, :edit, :update]
+
+
+  namespace :admin do
+    root to: 'dashboards#index'
+    get 'login', to: 'user_sessions#new'
+    post 'login', to: 'user_sessions#create'
+    delete 'logout', to: 'user_sessions#destroy'
+  end
 end
