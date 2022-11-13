23f6a280858e26fdf3c0986d49c84f08eb79dcd4
gentaro <going3956@yahoo.co.jp>
2022-11-13 14:01:55 +0900

    update: レビュー一覧が表示されるよう修正

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,16 +1,12 @@
-class UsersController < ApplicationController
+class Admin::UsersController < Admin::BaseController
   before_action :set_user, only: %i[edit update show destroy]
 
   def index
     @q = User.ransack(params[:q])
-    @users = @q.result(distinct: true).includes([:posts, :active_relationships, :passive_relationships]).order(id: :desc).page(params[:page])
+    @users = @q.result(distinct: true).order(created_at: :desc).page(params[:page])
   end
 
   def edit; end
-
-  def new
-    @user = User.new
-  end
 
   def update
     if @user.update(user_params)
@@ -21,28 +17,20 @@ class UsersController < ApplicationController
     end
   end
 
-  def create
-    @user = User.new(user_params)
-    if @user.save
-      auto_login(@user)
-      redirect_to login_path, success: t('.success')
-    else
-      flash.now[:danger] = t('.fail')
-      render :new
-    end
+  def show; end
+
+  def destroy
+    @user.destroy!
+    redirect_to admin_users_path, success: t('defaults.message.deleted', item: User.model_name.human)
   end
 
-  def set_user
-    @user = User.find(params[:id])
-  end
-  
   private
 
-  def user_params
-    params.require(:user).permit(:email, :password, :password_confirmation, :name)
-  end
-
   def set_user
     @user = User.find(params[:id])
+  end
+
+  def user_params
+    params.require(:user).permit(:email, :last_name, :first_name, :avatar, :avatar_cache, :role)
   end
 end
