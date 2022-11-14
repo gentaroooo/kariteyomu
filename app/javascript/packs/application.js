3199026a4855e701875d1e37a2eb02ba0008b2d5
gentaro <going3956@yahoo.co.jp>
2022-11-14 12:10:12 +0900

    update: herokupush10

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -4,7 +4,7 @@
 // that code so it'll be compiled.
 
 import Rails from "@rails/ujs"
-// import Turbolinks from "turbolinks"
+import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 // import "bootstrap";
@@ -13,5 +13,5 @@ import '@fortawesome/fontawesome-free/js/all';
 import "jquery";
 import "./preview_image";
 Rails.start()
-// Turbolinks.start()
+Turbolinks.start()
 ActiveStorage.start()
