0cec5e3501b7788831f56a7b3508aa2ceff455d7
gentaro <going3956@yahoo.co.jp>
2022-12-03 04:28:08 +0900

    add: seo

--- config/sitemap.rb
+++ config/sitemap.rb
@@ -2,26 +2,9 @@
 SitemapGenerator::Sitemap.default_host = "https://www.kariteyomu.net/"
 
 SitemapGenerator::Sitemap.create do
-  # Put links creation logic here.
-  #
-  # The root path '/' and sitemap index file are added automatically for you.
-  # Links are added to the Sitemap in the order they are specified.
-  #
-  # Usage: add(path, options={})
-  #        (default options are used if you don't specify)
-  #
-  # Defaults: :priority => 0.5, :changefreq => 'weekly',
-  #           :lastmod => Time.now, :host => default_host
-  #
-  # Examples:
-  #
-  # Add '/articles'
-  #
-  #   add articles_path, :priority => 0.7, :changefreq => 'daily'
-  #
-  # Add all articles:
-  #
-  #   Article.find_each do |article|
-  #     add article_path(article), :lastmod => article.updated_at
-  #   end
+  add root_path, priority: 1.0, changefreq: 'daily'
+  add posts_path, priority: 0.7, changefreq: 'daily'
+  Post.find_each do |post|
+    add post_path(post), lastmod: post.updated_at
+  end
 end
