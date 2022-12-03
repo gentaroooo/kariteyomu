1f5c3077b3f7445c12147b44eef4d19b834f217f
gentaro <going3956@yahoo.co.jp>
2022-12-04 06:53:42 +0900

    add: ゲストログインのテストを追加

--- spec/system/users/user_sessions_spec.rb
+++ spec/system/users/user_sessions_spec.rb
@@ -25,28 +25,6 @@ RSpec.describe 'UserSessions', type: :system do
       end
     end
 
-    describe 'ログイン' do
-    context '正常系' do
-      it 'ログインができる' do
-        visit login_path
-        fill_in 'メールアドレス', with: user.email
-        fill_in 'パスワード',	with: 'password'
-        click_button 'ログイン'
-        expect(page).to  have_content 'ログインしました'
-      end
-
-      it 'ログアウトができる' do
-        visit login_path
-        fill_in 'メールアドレス', with: user.email
-        fill_in 'パスワード',	with: 'password'
-        click_button 'ログイン'
-        expect(page).to  have_content 'ログインしました'
-        find(".dropdown-toggle").click
-        click_on 'ログアウト'
-        expect(page).to  have_content 'ログアウトしました'
-      end
-    end
-
     context '異常系' do
       it '入力が不足している場合、ログインできない' do
         visit login_path
@@ -55,6 +33,25 @@ RSpec.describe 'UserSessions', type: :system do
         click_button 'ログイン'
         expect(current_path).to eq login_path
         expect(page).to  have_content 'ログインに失敗しました'
+      end
+    end
+  end
+
+  describe 'ゲストログイン' do
+    context '正常系' do
+      it 'ログインができる' do
+        visit login_path
+        click_button 'ゲストログイン'
+        expect(page).to  have_content 'ログインしました'
+      end
+
+      it 'ログアウトができる' do
+        visit login_path
+        click_button 'ゲストログイン'
+        expect(page).to  have_content 'ログインしました'
+        find(".dropdown-toggle").click
+        click_on 'ログアウト'
+        expect(page).to  have_content 'ログアウトしました'
       end
     end
   end
